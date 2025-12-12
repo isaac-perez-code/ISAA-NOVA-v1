c20b826fb4e143c5dd2ec889e659ce98febce9e6
Isaa_Perez <fredisaacperezaspajo@gmail.com>
2025-12-11 23:27:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ export default {
     prefix: '.', // El prefijo para los comandos
     ownerNumber: '51983569753@s.whatsapp.net', // **TU NÚMERO** (Reemplazar con tu código de país y número)
     botName: 'ISAA-NOVA',
-    ownerName: 'Isaac Perez',
+    ownerName: 'Isaac Pérez',
     logoPath: `${__dirname}/media/logo.jpg`, // Asegúrate de guardar el logo en media/logo.jpg
 };
