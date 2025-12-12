db69484a3cf7064c24da1e33b7b5ad10d29905e3
Isaa_Perez <fredisaacperezaspajo@gmail.com>
2025-12-11 23:03:42 -0500

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    prefix: '.', // El prefijo para los comandos
+    ownerNumber: '51983569753@s.whatsapp.net', // **TU NÚMERO** (Reemplazar con tu código de país y número)
+    botName: 'ISAA-NOVA',
+    ownerName: 'Isaac Perez',
+    logoPath: `${__dirname}/media/logo.jpg`, // Asegúrate de guardar el logo en media/logo.jpg
+};
